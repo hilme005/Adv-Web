4fa027d08646e7bcdb5c8f7e0163738c47460038
hilme005 <46978287+hilme005@users.noreply.github.com>
2020-04-18 16:04:08 -0500

    Add files via upload

---
+++ 2_Surprise/surprise.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+$(".basketball").click(function() {
+	$(".madness").toggleClass("madness2");
+	$(".march").fadeIn("slow");
+	$(".sadness").fadeIn("slow");
+	$(".basketball").fadeOut("fast");
+	$(".shadow").fadeOut("fast");
+	
+});	
+
+
+});
