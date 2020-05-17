188e93488a77d2b6eaf92fc196d4241b02516e8a
hilme005 <46978287+hilme005@users.noreply.github.com>
2020-05-16 22:12:11 -0500

    Add files via upload

---
+++ main proj/intro.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+$(".button").click(function() {
+	$(".title").fadeIn("slow");
+	$(".title1").fadeIn("slow");
+	$(".title2").fadeIn("slow");
+	$(".title3").fadeIn("slow");
+	$(".title4").fadeIn("slow");
+	$(".title5").fadeIn("slow");
+	$(".title6").fadeIn("slow");
+	$(".title7").fadeIn("slow");
+	$(".title8").fadeIn("slow");
+	$(".waves").fadeOut("fast");
+	
+});	
+	
+});
+
+
